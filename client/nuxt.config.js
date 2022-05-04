94543fe294fd231c50b661288cbc88d88ab6e67e
kureharyosuke <ohhayo.com@gmail.com>
2022-05-04 15:56:10 +0900

    #1 install: vue nuxt axios @nuxtjs/axios

---
+++ client/nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  head: {
+    title: "nuxt-node",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+    ],
+  },
+  modules: ["@nuxtjs/vuetify", "@nuxtjs/axios"],
+  plugins: [],
+  vuetify: {},
+};
