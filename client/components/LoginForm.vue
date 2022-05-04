4311db5a3e744a575873a5e90decb4f0d6b3498f
kureharyosuke <ohhayo.com@gmail.com>
2022-05-04 20:35:22 +0900

    #1 update: LoginForm validation

--- client/components/LoginForm.vue
+++ client/components/LoginForm.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
     <v-card>
-      <v-form>
+      <v-form ref="form" v-model="valid" @submit="onSubmitLogin">
         <v-container>
-          <v-text-field label="이메일" type="email" required />
-          <v-text-field label="비밀번호" type="password" required />
-          <v-btn color="green" type="submit">로그인</v-btn>
+          <v-text-field v-model="email" label="이메일" type="email" :rules="emailRules" required />
+          <v-text-field v-model="password" label="비밀번호" type="password" :rules="passwordRules" required />
+          <v-btn color="green" :disabled="!valid" type="submit">로그인</v-btn>
           <v-btn nuxt to="/signup">회원가입</v-btn>
         </v-container>
       </v-form>
@@ -14,7 +14,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      valid: false,
+      email: "",
+      password: "",
+      emailRules: [(v) => !!v || "이메일을 입력해주세요."],
+      passwordRules: [(v) => !!v || "비밀번호를 입력해주세요."],
+    };
+  },
+  methods: {
+    onSubmitLogin() {
+      this.$refs.form.validation();
+    },
+  },
+};
 </script>
 
 <style></style>
