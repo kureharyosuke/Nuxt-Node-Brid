600b9bedf5a176574a78ef126dc9592ab5c03915
kureharyosuke <ohhayo.com@gmail.com>
2022-05-12 00:08:55 +0900

    add: mapState

--- client/store/users.js
+++ client/store/users.js
@@ -13,7 +13,9 @@ export const actions = {
   // context 의 구조분해를 사용하여 액션을 작성할 수 있다.
   // signUp({ commit, dispatch, state, getters, rootState, rootGetters }, payload) {},
   signUp({ commit }, payload) {
+    console.log("🚀 ~ file: users.js ~ line 16 ~ signUp ~ payload", payload);
     // 서버에 회원가입 요청을 보내는 부분
+    // context.commit("setMe", payload);
     commit("setMe", payload);
   },
   logIn({ commit }, payload) {
